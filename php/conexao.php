d4d17d4fb87bbe82c1d0f2578bc2f712d91b4afb
Izabelly Armeris <clailanearmeris@gmail.com>
2025-06-22 16:50:00 -0300

    Implementa conexão com DB via DATABASE_URL para compatibilidade com Railway

--- php/conexao.php
+++ php/conexao.php
@@ -1,16 +1,28 @@
 <?php
 
-$local_host = '127.0.0.1'; 
-$local_db   = 'nuvvo_db'; 
-$local_user = 'root';
-$local_pass = '';
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
 
+$database_url = getenv('DATABASE_URL');
 
-$host = getenv('MYSQLHOST') ?: $local_host;
-$db   = getenv('MYSQLDATABASE') ?: $local_db;
-$user = getenv('MYSQLUSER') ?: $local_user;
-$pass = getenv('MYSQLPASSWORD') ?: $local_pass;
-$port = getenv('MYSQLPORT') ?: '3306'; 
+if ($database_url) {
+  
+    $url_parts = parse_url($database_url);
+    
+    $host = $url_parts['host'];
+    $user = $url_parts['user'];
+    $pass = $url_parts['pass'];
+    $db   = ltrim($url_parts['path'], '/');
+    $port = $url_parts['port'];
+
+} else {
+   
+    $host = '127.0.0.1'; 
+    $db   = 'nuvvo_db';  
+    $user = 'root';
+    $pass = '';
+    $port = '3306';
+}
 
 $charset = 'utf8mb4';
 $dsn = "mysql:host=$host;port=$port;dbname=$db;charset=$charset";
@@ -22,9 +34,11 @@ $options = [
 ];
 
 try {
+   
     $pdo = new PDO($dsn, $user, $pass, $options);
 } catch (\PDOException $e) {
-    error_log("Erro de Conexão com o Banco de Dados: " . $e->getMessage());
+   
+    error_log("DATABASE CONNECTION ERROR: " . $e->getMessage());
     die("Desculpe, estamos com problemas técnicos. Tente novamente mais tarde.");
 }
 ?>
